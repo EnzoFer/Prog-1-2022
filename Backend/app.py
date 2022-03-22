8ad431bfa1cc908bfbcc6a745895b5d321dcbdcd
EnzoFer <enzo.fernandez@hotmail.com>
2022-03-22 20:00:29 -0300

    primer commit

--- Backend/app.py
+++ Backend/app.py
@@ -1,9 +1,8 @@
-from flask import Flask
-from dotenv import load_dotenv
 import os
+from main import create_app
 
-
-app = Flask (__name__)
+app = create_app()
+app.app_context().push()
 
 if __name__ == '__main__':
-    app.run(debug=True, port= 8000)
+    app.run(debug = True, port = os.getenv("PORT"))

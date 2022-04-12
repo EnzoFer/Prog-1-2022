0107bc6af240efb5291a5827124dab20ff4a5374
EnzoFer <enzo.fernandez@hotmail.com>
2022-04-12 20:12:29 -0300

    12/4/2022

---
+++ Backend/main/models/User.py
@@ -0,0 +1,45 @@
+from .. import db
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    rol = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    def __repr__(self):
+        return '<Usuario: %r %r >' % (self.firstname, self.password, self.rol, self.email)
+    #Convertir objeto en JSON
+    def to_json(self):
+        usuario_json = {
+            'id': self.id,
+            'firstname': str(self.firstname),
+            'password': str(self.password),
+            'rol' : str(self.rol),
+            'email': str(self.email),
+
+        }
+        return usuario_json
+
+    def to_json_short(self):
+        usuario_json = {
+            'id': self.id,
+            'firstname': str(self.firstname),
+            'password': str(self.password),
+            'rol' : str(self.rol),
+            'email': str(self.email),
+
+        }
+        return usuario_json
+    @staticmethod
+    #Convertir JSON a objeto
+    def from_json(usuario_json):
+        id = usuario_json.get('id')
+        firstname = usuario_json.get('firstname')
+        password = usuario_json.get('password')
+        rol = usuario_json.get('rol')
+        email = usuario_json.get('email')
+        return Users (id = id,
+            firstname = firstname,
+            password = password,
+            rol = rol,
+            email =email)
---
+++ Backend/main/models/__init__.py
@@ -0,0 +1,3 @@
+from .Poem import Poem as PoemModel
+from .User import User as UserModel
+from .Califi import Mark as MarkModel
